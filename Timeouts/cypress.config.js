3774971fea0f11558dd2865309a1a0010ba986e8
lazarmatrak <matrak.lazar@gmail.com>
2023-08-25 14:06:25 +0200

    add config folder with config file for different environments

--- Timeouts/cypress.config.js
+++ Timeouts/cypress.config.js
@@ -1,8 +1,24 @@
 const { defineConfig } = require("cypress");
+const fs = require("fs-extra");
+const path = require("path");
+
+function getConfigurationByFile(file) {
+  const pathToConfigFile = path.resolve("cypress\\config", `${file}.json`);
+
+  if (!fs.existsSync(pathToConfigFile)) {
+    console.log("No custom config file found!");
+    return {};
+  }
+
+  return fs.readJson(pathToConfigFile);
+}
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
+      const file = config.env.configFile || "";
+
+      return getConfigurationByFile(file);
       // implement node event listeners here
     },
     reporter: "cypress-multi-reporters",
