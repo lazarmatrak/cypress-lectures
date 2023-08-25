68e17cbebc7d83750a372d07d4b787aed56e4f7d
lazarmatrak <matrak.lazar@gmail.com>
2023-08-25 10:16:41 +0200

    made changes to PO file and spec

--- Timeouts/cypress/e2e/webdriver-uni/contact-us.cy.js
+++ Timeouts/cypress/e2e/webdriver-uni/contact-us.cy.js
@@ -1,21 +1,24 @@
 ///<reference types="cypress" />
 
 import HomepagePO from "../../pageObjects/Homepage_PO";
-
+import Contact_Us_PO from "../../pageObjects/Contact_Us_PO";
 describe('Test Contact Us form via WebDriverUni',()=>{
 
     // All of the tests that are stored inside this describe block will have a timeout of 20s - this includes the hooks as well
     Cypress.config('defaultCommandTimeout',20000);
 
     const homepage_PO = new HomepagePO();
-
+    const contact_Us_PO = new Contact_Us_PO();
 
     beforeEach(()=>{
         homepage_PO.visitHomepage();
+        cy.wait(3000);
         homepage_PO.clickOn_ContactUs_Button();
+        // cy.pause();
     });
 
     it('visits and asserts URL',()=>{
-        cy.url().should('contain',`${Cypress.env('baseUrl')}`)
+        cy.url().should('contain',`${Cypress.env('baseUrl')}`);
+        contact_Us_PO.contactForm_Submission('Lazar','Matrak','lazar@gmail.com','Some kind of comment','h1','Contact Us');
     })
 });
--- Timeouts/cypress/pageObjects/Contact_Us_PO.js
+++ Timeouts/cypress/pageObjects/Contact_Us_PO.js
@@ -2,7 +2,7 @@ class Contact_Us_PO{
     contactForm_Submission(firstName,lastName,email,comment,$selector,textToLocate){
         cy.get('[name="first_name"]').type(firstName);
         cy.get('[name="last_name"]').type(lastName);
-        cy.get('[name="email]').type(email);
+        cy.get('[name="email"]').type(email);
         cy.get('textarea.feedback-input').type(comment);
         cy.get($selector).contains(textToLocate,{timeout:15000});
     }
