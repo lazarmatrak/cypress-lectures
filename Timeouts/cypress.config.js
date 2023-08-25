4e4c0969f3bfec7621e87c0b8f97c7454b512a2e
lazarmatrak <matrak.lazar@gmail.com>
2023-08-25 11:33:26 +0200

    add notes.txt and configure cypress.config.js to support multi-reporters

--- Timeouts/cypress.config.js
+++ Timeouts/cypress.config.js
@@ -5,13 +5,17 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    env:{
-      baseUrl:'http://www.webdriveruniversity.com'
+    reporter: "cypress-multi-reporters",
+    reporterOptions: {
+      configFile: "reporter-config.json",
     },
-    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+    env: {
+      baseUrl: "http://www.webdriveruniversity.com",
+    },
+    specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
     pageLoadTimeout: 120000,
     defaultCommandTimeout: 10000,
     viewportHeight: 1080,
-    viewportWidth: 1920
+    viewportWidth: 1920,
   },
 });
