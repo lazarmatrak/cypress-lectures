b069500cae5e9d031dfdccc8b24e84045a1410b8
lazarmatrak <matrak.lazar@gmail.com>
2023-09-04 15:53:34 +0200

    add GET request local json server for posts

--- ApiXhrTesting/getRequest.cy.js
+++ ApiXhrTesting/getRequest.cy.js
@@ -1,1 +1,20 @@
 /// <reference types="cypress" />
+
+describe('API Requests',()=>{
+
+    it('GET request',()=>{
+        cy.request({
+            method: "GET",
+            url: "http://localhost:3000/posts",
+            headers:{
+                accept: "application/json"
+            }
+        }).then(response=>{
+            let body = JSON.parse(JSON.stringify(response.body));
+            console.log(body);
+
+            expect(body[0]).has.property('title','Example Json Server');
+            expect(body[0]).has.property('author','Joe Blogs');
+        });
+    })
+});
