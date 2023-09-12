02cdf149fb5d6631e717d806d6091986cd903f00
lazarmatrak <matrak.lazar@gmail.com>
2023-09-12 16:09:22 +0200

    add GET request to network-requests.cy.js

--- ApiXhrTesting/cypress/e2e/network-requests/network-requests.cy.js
+++ ApiXhrTesting/cypress/e2e/network-requests/network-requests.cy.js
@@ -1,17 +1,28 @@
 /// <reference types="cypress" />
 
-describe('Network Requests',()=>{
-    beforeEach(()=>{
-        cy.visit(Cypress.env('baseUrl'));
-    });
-    it('User is on localhost:4200',()=>{
-        cy.url().should('contain','localhost:4200');
-    });
+describe("Network Requests", () => {
+  // beforeEach(()=>{
+  //     cy.visit(Cypress.env('baseUrl'));
+  // });
+  // it('User is on localhost:4200',()=>{
+  //     cy.url().should('contain','localhost:4200');
+  // });
 
-    it('GET request',()=>{
-        cy.intercept({
-            method: 'GET',
-            url: Cypress.env('baseUrl')
-        })
-    })
-})
+  it("GET request", () => {
+    cy.request({
+      method: "GET",
+      url: Cypress.env("baseUrl"),
+    }).then((res) => {
+      let body = res.body;
+      body.forEach(($item) => {
+        cy.wrap($item).its("post").as("post");
+        cy.get("@post").each(($post) => {
+          console.log($post.id);
+        });
+        cy.wrap($item).should("have.property", "post");
+        cy.wrap($item).should("have.property", "comments");
+        cy.wrap($item).should("have.property", "author");
+      });
+    });
+  });
+});
